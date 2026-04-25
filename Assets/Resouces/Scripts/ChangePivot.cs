342b77e6be2eb6f85eac92c7bc5e370d51d3fee3
flavius841 <flaviusepintilie@gmail.com>
2026-04-25 13:38:26 +0300

    switching the pivot

--- Assets/Resouces/Scripts/ChangePivot.cs
+++ Assets/Resouces/Scripts/ChangePivot.cs
@@ -5,8 +5,8 @@ using System.Collections.Generic;
 public class ChangePivot : MonoBehaviour
 {
     [SerializeField] List<GameObject> Pivots = new List<GameObject>();
-    [SerializeField] float Distance;
     [SerializeField] float MaxDistance;
+    [SerializeField] bool SpacePressed;
     void Start()
     {
         GameObject[] FoundPivots = GameObject.FindGameObjectsWithTag("Pivot");
@@ -16,7 +16,53 @@ public class ChangePivot : MonoBehaviour
 
     void Update()
     {
+        CheckDistance();
+
+        if (Input.GetKeyDown(KeyCode.Space) && !SpacePressed)
+        {
+            SwitchPivot();
+            SpacePressed = true;
+        }
+
+        else if (Input.GetKeyUp(KeyCode.Space))
+        {
+            HingeJoint2D MyJoint = GetComponent<HingeJoint2D>();
+            MyJoint.connectedBody = null;
+            SpacePressed = false;
+        }
+
+    }
 
 
+    void CheckDistance()
+    {
+        MaxDistance = 0;
+
+        for (int i = 0; i < Pivots.Count; i++)
+        {
+            float Distance = Vector2.Distance(transform.position, Pivots[i].transform.position);
+
+            if (Distance > MaxDistance)
+            {
+                MaxDistance = Distance;
+            }
+        }
+    }
+
+    void SwitchPivot()
+    {
+        for (int i = 0; i < Pivots.Count; i++)
+        {
+            float Distance = Vector2.Distance(transform.position, Pivots[i].transform.position);
+
+            if (MaxDistance == Distance)
+            {
+                HingeJoint2D MyJoint = GetComponent<HingeJoint2D>();
+                Rigidbody2D Newtarget = Pivots[i].GetComponent<Rigidbody2D>();
+
+                MyJoint.connectedBody = Newtarget;
+
+            }
+        }
     }
 }
