a2ae1f826b616f9fd3f860be0f20b4302c1b1335
flavius841 <flaviusepintilie@gmail.com>
2026-05-08 17:40:59 +0300

    button animation

--- Assets/Resouces/Scripts/Animation.cs
+++ Assets/Resouces/Scripts/Animation.cs
@@ -1,36 +1,47 @@
 using UnityEngine;
 using PrimeTween;
+using TMPro;
 
 public class Animation : MonoBehaviour
 {
     public bool pressed;
     public bool selected;
+    public bool StopAnimation;
+
     void Start()
     {
 
-        // Tween.Scale(transform, 1.3f, 0.5f);
-        // Tween.Scale(transform, 0.9f, 0.5f);
-        // Tween.Scale(transform, 1.1f, 0.5f);
-        // Tween.Scale(transform, 1f, 0.5f);
     }
 
     void Update()
     {
         if (pressed)
         {
-            Tween.PunchScale(transform, Vector3.one * 0.3f, duration: 0.5f, frequency: 3);
-            pressed = false;
+            transform.GetChild(0).gameObject.SetActive(true);
+            SetAlpha(1);
+            if (!StopAnimation)
+            {
+                Tween.PunchScale(transform, Vector3.one * 0.3f, duration: 0.5f, frequency: 3);
+                StopAnimation = true;
+            }
+        }
+
+        else
+        {
+            transform.GetChild(0).gameObject.SetActive(false);
+            SetAlpha(0.7f);
         }
 
         if (selected)
         {
-            // Tween.Scale(transform, 1.3f, 0.5f);
-            // Tween.Scale(transform, 0.9f, 0.5f);
-            // Tween.Scale(transform, 1.1f, 0.5f);
-            // Tween.Scale(transform, 1f, 0.5f);
-            // selected = false;
+            Tween.Scale(transform, 1.4f, 0.5f);
         }
+    }
 
-
+    void SetAlpha(float alpha)
+    {
+        Color c = transform.GetChild(1).GetComponent<TextMeshProUGUI>().color;
+        c.a = alpha;
+        transform.GetChild(1).GetComponent<TextMeshProUGUI>().color = c;
     }
 }
